61e6dc8db8eb8230352d50c1ad1decf1d2fa277f
Jan Raring <janraring@yahoo.de>
2026-03-07 12:27:55 +0100

    added script for running the gui

---
+++ scripts/classification_gui.py
@@ -0,0 +1,42 @@
+from functools import partial
+from random import shuffle
+from enum import Enum
+
+from zettelsortierung.db import DataBase
+from zettelsortierung.DataTypes import Classifiers, TopCategory, Zettel
+from zettelsortierung.ClassificationGUI import run_classification
+
+db = DataBase()
+
+
+def get_unclassified() -> list[Zettel]:
+    already_done = db.get_classified_ids(Classifiers.GOLD, TopCategory)
+    zettels = [z for z in db.get_zettel() if z.id not in already_done]
+    shuffle(zettels)
+    return zettels
+
+
+def get_previous(label: Enum) -> list[Zettel]:
+    classifications = db.get_classifications(Classifiers.GOLD, TopCategory)
+    skipped_ids = {
+        zid for zid, probs in classifications.items()
+        if probs.get(label, 0.0) == 1.0
+    }
+    return db.get_zettel_by_ids(skipped_ids)
+
+
+def main():
+    queries = {
+        'Unclassified': get_unclassified,
+        'Skipped':      partial(get_previous, TopCategory.SKIP),
+        'Lautschrift':  partial(get_previous, TopCategory.LAUTSCHRIFT),
+        'Fragebogen':   partial(get_previous, TopCategory.FRAGEBOGEN),
+        'Wortschatz':   partial(get_previous, TopCategory.WORTSCHATZ),
+        'Sonstige':     partial(get_previous, TopCategory.SONSTIGE),
+    }
+
+    callback = partial(db.save_classification, Classifiers.GOLD)
+    run_classification(queries, TopCategory, on_classify=callback)
+
+if __name__ == '__main__':
+    main()

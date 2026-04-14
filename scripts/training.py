9cc8198cca4572df13b85f86e8018ed882c22c14
Jan Raring <janraring@yahoo.de>
2026-04-14 14:28:16 +0200

    made training script generic

--- scripts/training.py
+++ scripts/training.py
@@ -2,6 +2,7 @@ import os
 from dotenv import load_dotenv
 from zettelsortierung.nn import (
     SixtySamples,
+    ParquetDataset,
     TrainingConfig,
     Trainer,
     MobileNetV3ModelSmall,
@@ -11,8 +12,14 @@ load_dotenv()
 
 
 def main():
-    train_set = SixtySamples(train=True)
-    test_set = SixtySamples(train=False)
+    root = os.getenv("PROJECT_ROOT")
+    if root is None:
+        root = ""
+    dataset_name = "136_classes"
+    data_path = root + f"/data/interim/{dataset_name}.parquet"
+
+    train_set = ParquetDataset(data_path, train=True)
+    test_set = ParquetDataset(data_path, train=False)
     num_classes = len(train_set.get_classes())
     config = TrainingConfig(num_classes)
     model = MobileNetV3ModelSmall(num_classes)
@@ -21,9 +28,10 @@ def main():
     )
     trainer.train()
 
-    path = os.getenv("PROJECT_ROOT")
-    name = "mobile_net_v3_small_20ep_on_sixty_samples"
-    trainer.save(f"{path}/models/{name}")
+    # path = os.getenv("PROJECT_ROOT")
+    name = f"mobile_net_v3_small_{config.num_epochs}ep_on_{dataset_name}"
+    # name = "mobile_net_v3_small_20ep_on_sixty_samples"
+    trainer.save(f"{root}/models/{name}")
 
 
 if __name__ == "__main__":

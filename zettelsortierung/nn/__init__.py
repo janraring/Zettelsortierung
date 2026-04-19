1629c9a4f64a71fed5d7d322dcd9b7fcf4968d34
Jan Raring <janraring@yahoo.de>
2026-04-19 20:00:40 +0200

    added transformations

--- zettelsortierung/nn/__init__.py
+++ zettelsortierung/nn/__init__.py
@@ -1,7 +1,7 @@
 from .datasets.base import BaseDocumentDataset
 from .datasets.sixty_samples import SixtySamples
 from .datasets.parquet_dataset import ParquetDataset
-from .datasets.transforms import mobile_net_train_transform
+from .datasets.transforms import mobile_net_train_transform, mobile_net_infer_transform
 from .models.base import BaseModel
 from .models.mobilenet import MobileNetV3ModelSmall
 from .training.trainer import Trainer

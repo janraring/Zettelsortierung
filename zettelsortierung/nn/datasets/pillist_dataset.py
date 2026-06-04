ab8b80bce5e4658b6b35c3a2a0e96ca8f3b200c4
Jan Raring <janraring@yahoo.de>
2026-06-04 13:55:34 +0200

    New type of dataset

---
+++ zettelsortierung/nn/datasets/pillist_dataset.py
@@ -0,0 +1,19 @@
+from torch.utils.data import Dataset
+from torchvision import transforms
+from PIL import Image
+from zettelsortierung.DataTypes import Zettel
+
+
+class PILListDataset(Dataset):
+    def __init__(self, zettelpaths: list[str], transform=None):
+        self.zettelpaths = zettelpaths
+        self.transform = transform or transforms.ToTensor()
+
+    def __len__(self):
+        return len(self.zettelpaths)
+
+    def __getitem__(self, idx):
+        path = self.zettelpaths[idx]
+        img = Image.open(path)
+        transformed = self.transform(img)
+        return path, transformed
